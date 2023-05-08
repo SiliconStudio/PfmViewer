5c58609419c215d8f9c921e36d92bafe13b2b477
Vivien Oddou <vivien.oddou@siliconstudio.co.jp>
2023-05-08 19:07:07 +0900

    Initial commit for pfm viewer app

---
+++ cmake_suggestion_ninja_cl.bat
@@ -0,0 +1,3 @@
+echo execute in x64 native command prompt for vs or run "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+cmake -B build -G Ninja -DCMAKE_C_COMPILER="cl.exe" -DCMAKE_CXX_COMPILER="cl.exe" -DMSVC_TOOLSET_VERSION=140
+echo cd build && ninja
