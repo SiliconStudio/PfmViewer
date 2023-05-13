c234f24c7e701a2afe29b054c2fff85612b216a3
Vivien Oddou <vivien.oddou@siliconstudio.co.jp>
2023-05-13 15:24:30 +0900

    Cosmetics

--- cmake_suggestion_ninja_cl.bat
+++ cmake_suggestion_ninja_cl.bat
@@ -1,3 +1,3 @@
 echo execute in x64 native command prompt for vs or run "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-cmake -DCMAKE_BUILD_TYPE=Debug -B build -G Ninja -DCMAKE_C_COMPILER="cl.exe" -DCMAKE_CXX_COMPILER="cl.exe" -DMSVC_TOOLSET_VERSION=140
+cmake -DCMAKE_BUILD_TYPE=Release -B build -G Ninja -DCMAKE_C_COMPILER="cl.exe" -DCMAKE_CXX_COMPILER="cl.exe" -DMSVC_TOOLSET_VERSION=140
 echo "cd build && ninja"
